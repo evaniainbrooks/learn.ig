70e5e1e083d972ac75ef454a66c7c5689b054940
Evan Brooks <evaniainbrooks@gmail.com>
2019-01-21 16:28:14 +0300

    clean up caching

--- application.rb
+++ application.rb
@@ -6,25 +6,41 @@ require 'json'
 require 'slim'
 require 'yaml'
 require 'mime/types'
-
 require 'dotenv'
+require "google/cloud/translate"
+
 Dotenv.load
 
 PEXELS_API_KEY = ENV.fetch('PEXELS_API_KEY')
 COMMON_NOUNS = YAML.load_file('nouns.yml')
 
+# Your Google Cloud Platform project ID
+GOOGLE_PROJECT_ID = "learn-ig-228920"
+GOOGLE_CREDENTIALS = [File.dirname(__FILE__), "learn-ig.json"].join('/')
+
+BASE64_REGEXP = /\Adata:([-\w]+\/[-\w\+\.]+)?;base64,(.*)/m
+
+def cache(key, &block)
+  full_cache_key = [File.dirname(__FILE__), 'cache' , key].join('/')
+  return File.read(full_cache_key) if File.exist?(full_cache_key)
+
+  block.call.tap do |result|
+    File.open(full_cache_key, 'w') do |file|
+      file.write(result)
+      file.close
+    end
+  end
+end
+
+# Search for stock images
 def search_pexels(query)
   return nil unless query.length > 1
 
   normalized_query = query.gsub(' ', '+');
-  cache_key = File.dirname(__FILE__) + "/cache/pexels/#{normalized_query}.json"
-  result = File.read(cache_key) if File.exist?(cache_key)
+  cache_key = "pexels/#{normalized_query}.json"
 
-  puts "Cache hit #{normalized_query}" if result
-  puts "Cache miss #{normalized_query}" unless result
-
-  result ||=
-    begin
+  begin
+    cache(cache_key) do
       uri = URI('https://api.pexels.com/v1/search')
       uri.query = ["query=#{normalized_query}", "per_page=30", "page=1"].compact.join('&')
       http = Net::HTTP.new(uri.host, uri.port)
@@ -33,43 +49,31 @@ def search_pexels(query)
       req = Net::HTTP::Get.new(uri.request_uri, { 'Content-Type' =>'application/json', 'Authorization' => PEXELS_API_KEY })
       res = http.request(req)
 
-      res.body.tap do |body|
-        File.open(cache_key, 'w') do |file|
-          file.write(body)
-          file.close
-        end
-      end
-
-    rescue => e
-      puts "failed #{e}"
-      nil
+      JSON.parse(res.body)
     end
-
-  JSON.parse(result) if result
+  rescue => e
+    puts "Request failed #{e}"
+    nil
+  end
 end
 
-# Imports the Google Cloud client library
-require "google/cloud/translate"
-
+# Google translate text
 def translate_text(text, target: 'el', from: 'en')
-  # Your Google Cloud Platform project ID
-  project_id = "learn-ig-228920"
-  credentials = "/home/evan/Workspace/learn.ig/learn-ig.json"
+  cache_key = "translate/#{text.gsub(' ', '+')}+#{target}+#{from}.txt"
+  translate_client = Google::Cloud::Translate.new project: GOOGLE_PROJECT_ID, credentials: GOOGLE_CREDENTIALS
 
-  # Instantiates a client
-  translate_client = Google::Cloud::Translate.new project: project_id, credentials: credentials
+  cache(cache_key) do
+    translate_client.translate(text, to: target, from: from)
+  end
+end
 
-  cache_key = File.dirname(__FILE__) + "/cache/translate/#{text.gsub(' ', '+')}+#{target}+#{from}.txt"
-  result = File.read(cache_key) if File.exist?(cache_key)
+def save_image(name, target, data)
+  data_parts = data.match(BASE64_REGEXP) || []
+  extension = MIME::Types[data_parts[1]].first.preferred_extension
+  filename = "#{name}.#{extension}"
 
-  puts "Cache hit #{text}" if result
-  puts "Cache miss #{text}" unless result
-
-  translate_client.translate(text, to: target, from: from).tap do |result|
-    File.open(cache_key, 'w') do |file|
-      file.write(result)
-      file.close
-    end
+  File.open([File.dirname(__FILE__), 'images', target, filename].join('/'), 'wb') do |f|
+    f.write(Base64.decode64(data_parts[2]))
   end
 end
 
@@ -78,15 +82,7 @@ post '/:target/save_image' do
   @data = params[:ig_image_data]
   @target = params[:target]
 
-  REGEXP = /\Adata:([-\w]+\/[-\w\+\.]+)?;base64,(.*)/m
-
-  data_parts = @data.match(REGEXP) || []
-  extension = MIME::Types[data_parts[1]].first.preferred_extension
-  filename = "#{@name}.#{extension}"
-
-  File.open([File.dirname(__FILE__), 'images', @target, filename].join('/'), 'wb') do |f|
-    f.write(Base64.decode64(data_parts[2]))
-  end
+  save_image(@name, @target, @data)
 
   redirect "/#{@target}/generate/#{COMMON_NOUNS.sample}"
 end
@@ -95,10 +91,10 @@ get '/:target/generate/:query' do
   @query = params.fetch(:query, '')
   @query = "the " + @query if params.fetch(:pronoun, 1).to_i == 1
   @target = params.fetch(:target)
-  @result = search_pexels(@query.gsub('the ', ''))
   @translation = translate_text(@query, target: @target)
 
-  @photos = @result['photos'].collect do |photo|
+  result = search_pexels(@query.gsub('the ', ''))
+  @photos = result['photos'].collect do |photo|
     OpenStruct.new(photo['src'].transform_keys { |k| k.to_sym })
   end
 
